984c3a3085344c8a53fbbba006a343205538cdc6
itsdragonic <142061105+itsdragonic@users.noreply.github.com>
2024-05-26 19:34:11 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 var canvas = document.getElementById('main');
 var ctx = canvas.getContext('2d');
-var preload = document.getElementById('preload');
-var ctx2 = preload.getContext('2d');
 var buffer = document.getElementById('buffer');
 var ctx2 = buffer.getContext('2d');
 
@@ -29,49 +27,71 @@ function clearCanvas() {
 
 function redraw() {
     clearCanvas();
-  
+
+    let img = new Image;
+    if (physicalMap) {
+        img.src = "images/map.png";
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+
     // Draw Map
     ctx.drawImage(buffer, 0, 0, canvas.width, canvas.height);
+
+    let img2 = new Image;
+    if (physicalMap) {
+        img2.src = "images/map.png";
+        ctx.globalAlpha = 0.25;
+        ctx.drawImage(img2, 0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 1;
+
+        let img3 = new Image;
+        img3.src = "images/ocean.png";
+        ctx.drawImage(img3, 0, 0, canvas.width, canvas.height);
+    } else {
+        let img4 = new Image;
+        img4.src = "images/world.png";
+        ctx.drawImage(img4, 0, 0, canvas.width, canvas.height);
+    }
   
     // Draw Text
     ctx.fillStyle = 'white';
-    ctx.lineWidth = 1.5;
+    ctx.lineWidth = zoomAmount > 4 ? 1 : zoomAmount > 2 ? 2 : 3;
+
+    ctx.lineJoin = 'miter';
+    ctx.miterLimit = 2; // adjust
+    // ctx.lineJoin = 'round';
+    // ctx.lineJoin = 'bevel';
+    ctx.strokeStyle = '#a4ff11';
     ctx.strokeStyle = 'rgba(0, 0, 0, 0.9)';
   
     const nations = Object.keys(civs[timeline]).filter(nation => civs[timeline][nation].strength > 0);
   
     if (showNames) {
-        for (let i = 0; i < nations.length; i++) {
+        for (let i = nations.length - 1; i >= 0; i--) {
             const nation = nations[i];
             const civ = civs[timeline][nation];
-
+    
             ctx.font = (civ.size * 3 * canvas.width / buffer.width) + 'px carolingia';
-
+    
             const scaledX = civ.x * 0.96 * canvas.width / buffer.width;
-            const scaledY = civ.y * 1 * canvas.height / buffer.height;
-
+            const scaledY = civ.y * canvas.height / buffer.height;
+    
             ctx.strokeText(civ.name, scaledX, scaledY);
             ctx.fillText(civ.name, scaledX, scaledY);
         }
-    }
+    }    
 }  
 
 var zoomAmount = 1;
-window.onload = function() {	
-    	
+window.onload = function() {		
     redraw();
+    setInterval(() => {
+        redraw();
+    }, 500);
 
     var lastX=canvas.width/2, lastY=canvas.height/2;
 
     var dragStart,dragged;
-
-    canvas.addEventListener('mousedown',function(evt) {
-        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-        dragStart = ctx.transformedPoint(lastX,lastY);
-        dragged = false;
-    }, false);
 
     canvas.addEventListener('mousemove',function(evt) {
         lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
@@ -84,9 +104,19 @@ window.onload = function() {
         }
     }, false);
 
+    canvas.addEventListener('mousedown',function(evt) {
+        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+        dragStart = ctx.transformedPoint(lastX,lastY);
+        dragged = false;
+        canvas.style.cursor = 'move';
+    }, false);
+
     canvas.addEventListener('mouseup',function(evt) {
         dragStart = null;
         if (!dragged) zoom(evt.shiftKey ? -1 : 1 );
+        canvas.style.cursor = 'pointer';
     }, false);
 
     var scaleFactor = 1.1;
@@ -257,19 +287,25 @@ var showNews = true;
 // Checkboxes
 document.getElementById('phy').addEventListener('change', function() {
     physicalMap = this.checked;
-    console.log('Physical map status:', physicalMap);
     updateCivs();
     fallback();
 });
 document.getElementById('show').addEventListener('change', function() {
     showNames = this.checked;
-    console.log('Show names status:', showNames);
     updateCivs();
     fallback();
 });
 document.getElementById('news').addEventListener('change', function() {
     showNews = this.checked;
-    console.log('Show news status:', showNews);
     updateCivs();
     fallback();
+});
+document.getElementById('jagged').addEventListener('change', function() {
+    if (this.checked) {
+        canvas.style.clipPath = 'polygon(3% 0, 7% 1%, 11% 0%, 16% 2%, 20% 0, 23% 2%, 28% 2%, 32% 1%, 35% 1%, 39% 3%, 41% 1%, 45% 0%, 47% 2%, 50% 2%, 53% 0, 58% 2%, 60% 2%, 63% 1%, 65% 0%, 67% 2%, 69% 2%, 73% 1%, 76% 1%, 79% 0, 82% 1%, 85% 0, 87% 1%, 89% 0, 92% 1%, 96% 0, 98% 3%, 99% 3%, 99% 6%, 100% 11%, 98% 15%, 100% 21%, 99% 28%, 100% 32%, 99% 35%, 99% 40%, 100% 43%, 99% 48%, 100% 53%, 100% 57%, 99% 60%, 100% 64%, 100% 68%, 99% 72%, 100% 75%, 100% 79%, 99% 83%, 100% 86%, 100% 90%, 99% 94%, 99% 98%, 95% 99%, 92% 99%, 89% 100%, 86% 99%, 83% 100%, 77% 99%, 72% 100%, 66% 98%, 62% 100%, 59% 99%, 54% 99%, 49% 100%, 46% 98%, 43% 100%, 40% 98%, 38% 100%, 35% 99%, 31% 100%, 28% 99%, 25% 99%, 22% 100%, 19% 99%, 16% 100%, 13% 99%, 10% 99%, 7% 100%, 4% 99%, 2% 97%, 1% 97%, 0% 94%, 1% 89%, 0% 84%, 1% 81%, 0 76%, 0 71%, 1% 66%, 0% 64%, 0% 61%, 0% 59%, 1% 54%, 0% 49%, 1% 45%, 0% 40%, 1% 37%, 0% 34%, 1% 29%, 0% 23%, 2% 20%, 1% 17%, 1% 13%, 0 10%, 1% 6%, 1% 3%)';
+        canvas.style.boxShadow = '0 0 50px rgba(0, 0, 0, 0.9)';
+    } else {
+        canvas.style.clipPath = 'none';
+        canvas.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+    }
 });
